2bde7f2175d941472903caa3182074b9d49e4b45
Han <39874518+handoyo-wahyu@users.noreply.github.com>
2022-05-13 15:38:06 +0700

    Update QR_code_generator.py

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -1,41 +1,49 @@
+# Modules
+import cv2
 import streamlit as st
 import numpy as np
 import os
 import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
-import cv2
 
+# QR Code
 import qrcode
 qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=10,border=14)
 
+# Load image
 from PIL import Image
 def load_image(img):
     im = Image.open(img)
     return im
 
+# Application
 def main():
-    menu = ["Home", "DecodeQR", "Kelompok Y"]
-    
+    menu = ["Home", "Decode QR", "Kelompok Y"]
     choice = st.sidebar.selectbox("Menu", menu)
     
     if choice == "Home":
         st.subheader("Home")
         
+        # Text input
         with st.form(key='myqr_form'):
             raw_text = st.text_area("Text here")
             submit_button = st.form_submit_button("Generate")
 
-        if submit_button:
-            
-            col1, col2 = st.columns(2)
-            
-            with col1:
-                
+        # Layout
+        if submit_button:         
+            col1, col2 = st.columns(2)            
+            with col1:                
+                # Add data
                 qr.add_data(raw_text)
                 
+                # Generate QR
                 qr.make(fit=True)
                 img = qr.make_image(fill_color='black', back_color='white')
-                img_filename = 'QRimage_{}.png'.format(timestr)
+                
+                # Filename
+                img_filename = 'QR_image_{}.png'.format(timestr)
+                
+                # Save file
                 path_for_image = os.path.join('image_folder', img_filename)
                 img.save(path_for_image)
             
@@ -46,17 +54,12 @@ def main():
                 st.info("Original Text")
                 st.write(raw_text)
         
-    elif choice == "DecodeQR":
-        st.subheader("DecodeQR")
+    elif choice == "Decode QR":
+        st.subheader("Decode QR")
 
         image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg'])
         
-        if image_file is not None:
-            # Method 1
-            #img = load_image(image_file)
-            #st.image(img)
-            
-            # Method 2
+        if image_file is not None:            
             file_bytes = np.asarray(bytearray(image_file.read()),dtype=np.uint8)
             opencv_image = cv2.imdecode(file_bytes,1)
             
@@ -72,7 +75,9 @@ def main():
                 st.write(retval)
             
     else:
-        st.subheader("Kelompok Y")
-        
+        st.title("Kelompok Y")
+        st.write("1. Handoko Bimawidjaya - 152163240100-512")
+        st.write("2. Handoyo Wahyu Utomo - 152163240100-889")
+
 if __name__ == '__main__':
     main()
